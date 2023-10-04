4d90ed79645bc2a3173b2ddb4bb26150f53288df
stefanmallaun <stmallaun@tsn.at>
2023-10-04 14:38:08 +0200

    Lottoziehung fertig

--- Lottoziehung/Lottoziehung.py
+++ Lottoziehung/Lottoziehung.py
@@ -1,16 +1,56 @@
-from random import *
-from operator import *
+import random
+import matplotlib.pyplot as plt
+import numpy as np
 
-def ziehe_lottozahlen():
-    gezogene_zahlen = []
-    
-    while length_hint(gezogene_zahlen) < 6:
-        x = randrange(1,45)
-        if x not in gezogene_zahlen:
-            gezogene_zahlen.append(x)
-        
-    return gezogene_zahlen
+# Funktion, um Lottozahlen zu ziehen
+def gamble(n, res):
+    if n <= 0:
+        print("Die Anzahl der zu ziehenden Zahlen muss positiv sein.")
+        return
 
-lottozahlen = ziehe_lottozahlen()
-print(lottozahlen)
-        
+    max = 45
+    num = []
+
+    # Erstellt eine Liste von Zahlen von 0 bis 44
+    for i in range(max):
+        num.append(i)
+
+    for i in range(n):
+        # Zufällige Auswahl einer Indexposition aus der Liste
+        index = random.randint(0, max - 1 - i)
+
+        old = num[index]
+        new = num[max - 1 - i]
+
+        # Vertauscht die ausgewählte Zahl mit der letzten verbleibenden Zahl in der Liste
+        num[index], num[max - 1 - i] = new, old
+        res.append(old)
+
+    return res
+
+# Funktion, um die Häufigkeit jeder gezogenen Zahl zu zählen
+def statistic(res, dic):
+    for i in range(len(res)):
+        dic[res[i]] = dic[res[i]] + 1
+
+# Ein leeres Dictionary, um die Häufigkeiten der gezogenen Zahlen zu speichern
+dic = {}
+for x in range(45):
+    dic[x] = 0
+
+tries = 1000
+
+# Führt die Ziehung und die Statistik für eine bestimmte Anzahl von Versuchen (hier 1000) durch
+for n in range(tries):
+    res = []
+    # Zieht 6 Lottozahlen und speichert sie in der 'res'-Liste
+    gamble(6, res)
+    # Aktualisiert das Dictionary 'dic', um die Häufigkeiten der gezogenen Zahlen zu zählen
+    statistic(res, dic)
+
+# Erstellt ein Histogramm der gezogenen Zahlen
+plt.bar(dic.keys(), dic.values())
+plt.title("Häufigkeit der Lottozahlen, n=" + str(tries))
+plt.xlabel("Zahlen")
+plt.ylabel("Häufigkeit")
+plt.show()
