b98e3583ffc6227a9bbcde8fc04831c1a8385318
stefanmallaun <stmallaun@tsn.at>
2023-11-03 16:38:29 +0100

    Flush now working

--- Poker/poker.py
+++ Poker/poker.py
@@ -8,58 +8,44 @@
 #richtiger Anteil recherchieren und mit ergebnis vergleichen
 import random as r
 
-colors = ['red', 'black']
-suits = ['hearts', 'diamonds', 'clubs', 'spades']
-numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-
-def generate_deck():
-    deck = [f'{rank} of {suit}' for rank in numbers for suit in suits]
-    r.shuffle(deck)
-    return deck
-
-def deal_hand(deck):
-    return [deck.pop() for _ in range(5)]
-
-# Rest of your code remains the same
-
-
-# Rest of your code remains the same
+# Numerical representations for colors, suits, and numbers
+colors = [0, 1]  # 0 for black, 1 for red
+suits = [0, 1, 2, 3]  # 0 for clubs, 1 for diamonds, 2 for hearts, 3 for spades
+numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 def is_pair(listOfCards):
-    values = [card[-1] for card in listOfCards]
+    values = [card % 13 for card in listOfCards]  # Use modulo to get the number
     for value in values:
         if values.count(value) == 2:
             return True
     return False
 
 def is_three_of_a_kind(listOfCards):
-    values = [card[-1] for card in listOfCards]
+    values = [card % 13 for card in listOfCards]
     for value in values:
         if values.count(value) == 3:
             return True
     return False
 
 def is_four_of_a_kind(listOfCards):
-    values = [card[-1] for card in listOfCards]
+    values = [card % 13 for card in listOfCards]
     for value in values:
         if values.count(value) == 4:
             return True
     return False
 
-
-
 def is_royal_flush(listOfCards):
     if not is_flush(listOfCards):
         return False
-    
-    sorted_cards = sorted(listOfCards, key=lambda card: numbers.index(card[-1]))
-    royal_flush_values = ['A', 'K', 'Q', 'J', '10']
-    return [card[-1] for card in sorted_cards[-5:]] == royal_flush_values
+
+    sorted_cards = sorted(listOfCards, key=lambda card: card % 13)
+    royal_flush_values = [0, 11, 12, 10, 9]  # Numerical values for 'A', 'K', 'Q', 'J', '10'
+    return [card % 13 for card in sorted_cards[-5:]] == royal_flush_values
 
 def is_full_house(listOfCards):
     rank_count = {}
     for card in listOfCards:
-        rank = card[-1]
+        rank = card % 13
         if rank in rank_count:
             rank_count[rank] += 1
         else:
@@ -68,7 +54,7 @@ def is_full_house(listOfCards):
     return sorted(rank_count.values()) == [2, 3]
 
 def is_straight(cards):
-    card_values = [numbers.index(card[-1]) if card[-1] in numbers else 14 if card[-1] == 'A' else 13 if card[-1] == 'K' else 12 if card[-1] == 'Q' else 11 if card[-1] == 'J' else 10 if card[-1] == '10' else 0 for card in cards]
+    card_values = [card % 13 for card in listOfCards]
     
     sorted_cards = sorted(card_values)
     
@@ -79,17 +65,17 @@ def is_straight(cards):
     return True
 
 def is_flush(listOfCards):
-    first_suit = listOfCards[0][5:]
-    return all(card[5:] == first_suit for card in listOfCards)
+    first_suit = listOfCards[0] // 13
+    return all(card // 13 == first_suit for card in listOfCards)
 
 def is_straight_flush(cards):
     if not is_flush(cards):
         return False
     
-    sorted_cards = sorted(cards, key=lambda card: numbers.index(card[-1]))
+    sorted_cards = sorted(cards, key=lambda card: card % 13)
     
     for i in range(len(sorted_cards) - 1):
-        if numbers.index(sorted_cards[i][-1]) + 1 != numbers.index(sorted_cards[i + 1][-1]):
+        if sorted_cards[i] % 13 + 1 != sorted_cards[i + 1] % 13:
             return False
     
     return True
@@ -97,7 +83,7 @@ def is_straight_flush(cards):
 def is_two_pair(listOfCards):
     rank_count = {}
     for card in listOfCards:
-        rank = card[-1]
+        rank = card % 13
         if rank in rank_count:
             rank_count[rank] += 1
         else:
@@ -105,7 +91,6 @@ def is_two_pair(listOfCards):
 
     pair_counts = list(rank_count.values())
     return pair_counts.count(2) == 2
-
 
 def evaluate_hand(listOfCards):
     if is_royal_flush(listOfCards):
@@ -129,7 +114,6 @@ def evaluate_hand(listOfCards):
     else:
         return "High Card"
 
-
 # Initialize variables to count different combinations
 combinations_count = {
     "Royal Flush": 0,
@@ -144,31 +128,24 @@ combinations_count = {
     "High Card": 0,
 }
 
-
-total_simulations = 10000
+total_simulations = 1000000
 for _ in range(total_simulations):
     listOfCards = []
-    card = None
 
     while len(listOfCards) < 5:
-        c = r.randint(0, 1)
-        s = r.randint(0, 3)
+        c = r.choice(colors)
+        s = r.choice(suits)
         n = r.randint(0, 12)
 
-        # Only clubs and spades can be black
-        card = colors[c] + ' ' + suits[s] + ' ' + numbers[n]
+        card = c * 13 + s * 13 + n
 
-
-        if card not in listOfCards and card is not None:
+        if card not in listOfCards:
             listOfCards.append(card)
 
-    # Evaluate the poker hand
     hand_result = evaluate_hand(listOfCards)
 
-    # Count the combinations
     combinations_count[hand_result] += 1
 
-# Calculate the percentage of each combination
 combinations_percentage = {
     combination: count / total_simulations * 100 for combination, count in combinations_count.items()
 }
@@ -176,5 +153,5 @@ combinations_percentage = {
 # Display the results
 for combination, percentage in combinations_percentage.items():
     print(f"{combination}: {percentage:.2f}%")
-    
+
 print(combinations_count)
