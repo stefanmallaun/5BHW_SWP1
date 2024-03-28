4114f10aae56ea69ab66a7829e5fd3d6f0645b9b
stefanmallaun <stmallaun@tsn.at>
2024-03-29 00:08:54 +0100

    cleaned

--- Linkedlist/Linkedlist.py
+++ Linkedlist/Linkedlist.py
@@ -12,74 +12,72 @@ import random
     - iterator protokoll implementieren
 """
 
-
-
 class Node:
-    def __init__(self, data=None):
-        # Initialisierung eines neuen Knotens mit Daten und einer Verbindung zum nächsten Knoten (None, wenn es keinen nächsten Knoten gibt)
-        self.data = data
-        self.next = None
+	def __init__(self, data):
+		self.data = data
+		self.next = None
 
-class LinkedList:
+class LL:
+    
     def __init__(self):
-        # Initialisierung der leeren verketteten Liste mit einem leeren Kopf (head) und der Länge 0
         self.head = None
-        self.length = 0
-
-    def append(self, data):
-        # Hinzufügen eines Elements am Ende der verketteten Liste
+        self.current = None 
+        
+    def inserAtEnd(self, data):
         new_node = Node(data)
-        if not self.head:
-            # Wenn die Liste leer ist, wird das neue Element der Kopf der Liste
+        if self.head is None:
             self.head = new_node
+            self.current = new_node
+            return
+    
+        current_node = self.head
+        while(current_node.next):
+            current_node = current_node.next
+    
+        current_node.next = new_node
+    
+    
+    def sizeOfLL(self):
+        size = 0
+        if(self.head):
+            current_node = self.head
+            while(current_node):
+                size += 1
+                current_node = current_node.next
+            return size
         else:
-            # Andernfalls wird das Element an das Ende der Liste angehängt
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-        self.length += 1
-
-    def __len__(self):
-        # Überschreiben der Länge der verketteten Liste
-        return self.length
-
+            return 0
+        
+    def printLL(self):
+        current_node = self.head
+        while(current_node):
+            print(current_node.data)
+            current_node = current_node.next
+            
     def __iter__(self):
-        # Iterationsmethode für die verkettete Liste
-        self.current = self.head
         return self
 
     def __next__(self):
-        # Methode zum Fortsetzen der Iteration durch die Liste
-        if self.current:
+        if self.current is None:
+            raise StopIteration
+        else:
             data = self.current.data
             self.current = self.current.next
             return data
-        else:
-            raise StopIteration
-
-    def display(self):
-        # Methode zum Anzeigen der Elemente der verketteten Liste
-        elements = []
-        current = self.head
-        while current:
-            elements.append(current.data)
-            current = current.next
-        print("Liste: ", elements)
-
-if __name__ == "__main__":
-    # Hauptteil des Codes, der bei Ausführung direkt ausgeführt wird
-    linked_list = LinkedList()
+        
+def main():
+    Linked_list = LL()
     
-    # Hinzufügen von 10 zufälligen Elementen zur Liste
     for _ in range(10):
-        linked_list.append(random.randint(1, 100))
-
-    # Anzeigen der Länge der Liste und der Elemente
-    print("Länge der Liste:", len(linked_list))
-    linked_list.display()
-
-    # Iteration durch die Liste und Anzeigen der Elemente
-    print("\nDurch die Liste iterieren:")
-    for item in linked_list:
-        print(item)
+        rndm = random.randint(1,100)
+        Linked_list.inserAtEnd(rndm)
+        
+    print("LISTE\n")
+    Linked_list.printLL()
+    print("\nLänge: ")
+    
+    length = Linked_list.sizeOfLL()
+    print(length)
+    
+if __name__ == '__main__':
+    main()
